9e10d2a9acf6cdfbeda4e81631bb372f0c8e118c
finance2562envocc-debug <finance2562.envocc@gmail.com>
2026-03-23 08:52:59 +0700

    +

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   'use strict';
 
   global.DOC_CONTROL_CONFIG = {
-    scriptUrl: 'https://script.google.com/macros/s/AKfycbz1jajW3BCP1XBFCjgOdg440WooyoBMmtfdcrFUk2OINl0DzZhVfa1BqkF9Uquhh6E/exec',
+    scriptUrl: 'https://script.google.com/macros/s/AKfycbxKEEpv5F6o5USVdkj7oI8DyphoHWzTEiy4TR7BueCi_JTNpaxQzbtfDgUPYfdr83zd/exec',
     lockSettings: true
   };
 })(typeof window !== 'undefined' ? window : this);
